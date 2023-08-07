207c9f92c7e4143a22f7b348038639181ce00d1f
MaestroAshmin <ashmin.karki1996@gmail.com>
2023-08-07 18:47:11 +1000

    Logout API created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ class AuthController extends Controller
 {
     public function register(Request $request) {
         $fields = $request->validate([
-            'user_name' => 'required|string',
+            'user_name' => 'required|string|unique:users,user_name',
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed',
         ]);
@@ -21,6 +21,30 @@ class AuthController extends Controller
             'email' => $fields['email'],
             'password' => bcrypt($fields['password'])
         ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'user_name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('user_name', $fields['user_name'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Wrong Credentials'
+            ],401);
+        }
 
         $token = $user->createToken('myapptoken')->plainTextToken;
 
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 // Protected routes
 Route:: group(['middleware' => ['auth:sanctum']], function(){
